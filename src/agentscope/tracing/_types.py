5b5ed1b853deefe95ff74c72c13c7d758a2f5fee
潜星 <zhijian.mzj@alibaba-inc.com>
2025-10-28 18:19:23 +0800

    add docstring to type

--- src/agentscope/tracing/_types.py
+++ src/agentscope/tracing/_types.py
@@ -5,30 +5,43 @@ from enum import Enum
 class SpanAttributes:
     """The span attributes."""
 
-    SPAN_KIND = "span.kind"
-    OUTPUT = "output"
-    INPUT = "input"
-    META = "metadata"
-    PROJECT_RUN_ID = "project.run_id"
-
     GEN_AI_TOOL_CALL_ARGUMENTS = "gen_ai.tool.call.arguments"
+    """The gen ai tool call arguments."""
+
     GEN_AI_TOOL_CALL_RESULT = "gen_ai.tool.call.result"
+    """The gen ai tool call result."""
 
     AGENTSCOPE_FORMAT_INPUT = "agentscope.format.input"
+    """The agentscope format input."""
+
     AGENTSCOPE_FORMAT_OUTPUT = "agentscope.format.output"
+    """The agentscope format output."""
 
     AGENTSCOPE_FUNCTION_NAME = "agentscope.function.name"
+    """The agentscope function name."""
+
     AGENTSCOPE_FUNCTION_INPUT = "agentscope.function.input"
+    """The agentscope function input."""
+
     AGENTSCOPE_FUNCTION_OUTPUT = "agentscope.function.output"
+    """The agentscope function output."""
 
 
 class OperationNameValues(str, Enum):
     """The provider name values."""
+
     FORMATTER = "format"
+    """The formatter operation name."""
+
     INVOKE_GENERIC_FUNCTION = "invoke_generic_function"
+    """The invoke generic function operation name."""
 
 
 class ProviderNameValues(str, Enum):
     """The provider name values."""
+
     DASHSCOPE = "dashscope"
+    """The dashscope provider name."""
+
     OLLAMA = "ollama"
+    """The ollama provider name."""
